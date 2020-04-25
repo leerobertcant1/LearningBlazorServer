1ea942aa0c0b899511264f34afc552941dda805e
Lee Cant <lee.cant@ampetronic.co>
2020-04-25 21:28:45 +0100

    Added logger

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using LearningBlazorServer.Data;
+using Microsoft.Extensions.Logging;
 
 namespace LearningBlazorServer
 {
