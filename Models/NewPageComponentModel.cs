512cedc0553f2e3fb95b34a41ea6e229d681932d
Lee Cant <lee.cant@ampetronic.co>
2020-04-26 10:51:47 +0100

    Added a form with Validation

--- Models/NewPageComponentModel.cs
+++ Models/NewPageComponentModel.cs
@@ -1,8 +1,15 @@
-﻿namespace LearningBlazorServer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningBlazorServer.Models
 {
     public class NewPageComponentModel
     {
+        [Required]
+        [Range(0, 9999999, ErrorMessage ="Please select an ID")]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(999999999, MinimumLength = 1, ErrorMessage = "Please enter a value")]
         public string Value { get; set; }
     }
 }
