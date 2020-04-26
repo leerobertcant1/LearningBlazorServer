c479a1d61090d1fb75ce91bb5ed9501b1e9543e5
Lee Cant <lee.cant@ampetronic.co>
2020-04-26 11:52:41 +0100

    Added authentication, but Entity Framework core can't find the connection string, must be a bug

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LearningBlazorServer.Areas.Identity.Data;
+using LearningBlazorServer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LearningBlazorServer.Areas.Identity.IdentityHostingStartup))]
+namespace LearningBlazorServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LearningBlazorServerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LearningBlazorServerContextConnection")));
+
+                services.AddDefaultIdentity<LearningBlazorServerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LearningBlazorServerContext>();
+            });
+        }
+    }
+}
